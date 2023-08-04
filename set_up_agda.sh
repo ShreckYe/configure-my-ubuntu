f59dec364ae44fd4d835b4ff975ef686b862b459
Shreck Ye <ShreckYe@gmail.com>
2023-08-04 21:48:10 +0800

    Fix unescaped echos in set_up_agda.sh

--- set_up_agda.sh
+++ set_up_agda.sh
@@ -18,8 +18,8 @@ git clone https://github.com/agda/cubical.git
 cd ~
 mkdir .agda
 cd .agda
-echo "~/GitProjects/agda-stdlib/standard-library.agda-lib\n~/GitProjects/cubical/cubical.agda-lib\n" > libraries
-echo "standard-library\ncubical\n" > defaults
+echo -e "~/GitProjects/agda-stdlib/standard-library.agda-lib\n~/GitProjects/cubical/cubical.agda-lib\n" > libraries
+echo -e "standard-library\ncubical\n" > defaults
 cd ..
 
 echo "Remember to git checkout the library versions corresponding to the Agda version. See https://github.com/agda/agda-stdlib/blob/master/notes/installation-guide.md and https://github.com/agda/cubical/tree/master#compiling-using-and-installing."
