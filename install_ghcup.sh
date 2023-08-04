40ed762265773ff0743c98879e8cc7b844aea825
Shreck Ye <ShreckYe@gmail.com>
2023-08-04 14:59:40 +0800

    Add the required packages by GHCup

--- install_ghcup.sh
+++ install_ghcup.sh
@@ -1,2 +1,5 @@
 # https://www.haskell.org/ghcup/#
+
+sudo apt install -y build-essential curl libffi-dev libffi8ubuntu1 libgmp-dev libgmp10 libncurses-dev libncurses5 libtinfo5
+
 curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
