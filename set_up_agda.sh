a8c4d930b702ebf15cad5ac84d0d860784021b1f
Shreck Ye <ShreckYe@gmail.com>
2023-08-04 11:40:54 +0800

    Initially add 2 untested scripts to install GHCup and set up agda

---
+++ set_up_agda.sh
@@ -0,0 +1,23 @@
+# https://agda.readthedocs.io/en/latest/getting-started/installation.html
+
+sudo apt-get install zlib1g-dev libncurses5-dev
+
+cabal update
+cabal install Agda
+
+cd ~/GitProjects
+
+# https://github.com/agda/agda-stdlib/blob/master/notes/installation-guide.md
+gh repo clone agda/agda-stdlib
+
+# https://github.com/agda/cubical/blob/master/INSTALL.md
+gh repo clone agda/cubical
+
+cd ~
+mkdir .agda
+cd .agda
+echo "~/GitProjects/agda-stdlib/standard-library.agda-lib\n~/GitProjects/cubical/cubical.agda-lib\n" > libraries
+echo "standard-library\ncubical\n" > defaults
+cd ..
+
+echo "Remember to git checkout the library versions corresponding the Agda version. See https://github.com/agda/agda-stdlib/blob/master/notes/installation-guide.md and https://github.com/agda/cubical/tree/master#compiling-using-and-installing."
